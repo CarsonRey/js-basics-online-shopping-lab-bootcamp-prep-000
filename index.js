ac10a5aa5274e7eb9ab10baec13753a4ed30fe9b
Carson Rey <c.rey494@gmail.com>
2018-08-28 22:30:27 +0000

    Done.

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ function addToCart(item) {
  function getItemPrice(max) {
     return Math.floor(Math.random() * Math.floor(max));
  } 
-var newItem = {itemName:`${item}`, itemPrice: getItemPrice(100)};
+var newItem = {itemName:`${item}`, itemPrice: getItemPrice(100) + 1};
+
 cart.push(newItem)
-return cart;
+
+return `${item} has been added to your cart.`;
 }
 
 //"passed in string" needs to be dynamic --> hints at interpolation.
@@ -29,35 +31,57 @@ return cart;
 
 function viewCart() {
 var list = [];
-var i = 0;
- if (cart.length === 0) {
-    return `Your shopping cart is empty.`
-  }
-  
- else {
-
-   list.push(`In your cart, you have ${cart[i].itemName} at ${cart[i].itemPrice}`)
+if (cart.length === 0) {
+    return 'Your shopping cart is empty.'
 }
-    return list;
+for (var i = 0; i < cart.length; i++) {
+var itemListing = `${cart[i].itemName} at $${cart[i].itemPrice}`
+ 
+ if (cart.length === 1) {
+   list.push(`In your cart, you have ${itemListing}.`) 
+}
+
+ else if (cart.length === 2) {
+   list.push(`In your cart, you have ${itemListing} and ${itemListing}.`)
+}
+
+else{
+  list.push(`In your cart, you have ${itemListing}, ${itemListing}, and ${itemListing}.`)
+}
+}
+return list;
 }
 
 function total() {
-  
+  for (var i = 0; i < cart.length; i++){
+    
+    return 
+  }
 }
 
 function removeFromCart(item) {
   for (var i = 0; i < 0; i++) {
-  var firstItemName = getCart()[i].itemName
-  var secondItemName = getCart()[i].itemName
-  if (i > 0){}
-  else {}
+
+  if (item === undefined){
+    return `That item is not in your cart.`
+  }
+  else {
+    cart.splice(item[i], 1)
+    return cart
+  }
   }
 }
 
 function placeOrder(cardNumber) {
-  var total = [];
-  if (cardNumber === 0) {
-  return "Sorry we don't have a credit card on file for you."
+ function emptyCart() {
+    var cart = [];
+    return cart 
+ }
+  if (cardNumber > 0) {
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     }
-  
+  else {
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  console.log(emptyCart())  
 }
